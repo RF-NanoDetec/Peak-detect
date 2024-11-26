4da92309b621ae865939f44aa98526979f17b11a
Arthur-Conan-Doyle <silasweiss1999@gmail.com>
2024-11-27 00:17:59 +0100

    Create _main_.py
    I added the first variant of the necessary code for this section.

---
+++ peak_analysis_tool/peak_analysis/_main_.py
@@ -0,0 +1,9 @@
+"""
+Entry point for running the package as a module.
+Allows running the application with 'python -m peak_analysis'.
+"""
+
+from .main import main
+
+if __name__ == "__main__":
+    main()
